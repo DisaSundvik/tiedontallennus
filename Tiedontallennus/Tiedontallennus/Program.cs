75b4b46f051a912a061ddf714084726a23eaf8f5
Disa Sundvik <disa.sundvik@stud.pointcollege.fi>
2018-05-16 13:20:55 +0300

    Lisätty aiemman lämpötilan lukeminen

--- Tiedontallennus/Tiedontallennus/Program.cs
+++ Tiedontallennus/Tiedontallennus/Program.cs
@@ -11,14 +11,27 @@ namespace Tiedontallennus
     {
         static void Main(string[] args)
         {
+      
+            string tiedosto = @"F:\Lampotila.txt";
+
+            //luetaan tiedostosta aiemmin asetettu lämpötila
+            if (File.Exists(tiedosto))
+            {
+                string aiempiArvo = File.ReadAllText(tiedosto);
+                Console.WriteLine("Aiemmin asetettu lämpötila on" + aiempiArvo + " astetta. ");
+            }
+            else
+            {
+                Console.WriteLine("Tervetuloa! ");
+            }
+
             int Lampotila = 0;
             Console.Write("Anna uusi lämpötila: ");
             string syöte = Console.ReadLine();
             Lampotila = int.Parse(syöte);
 
-            //tiedostoon kirjoittaminen
-            string tiedosto = @"F:\Lampotila.txt";
 
+            //tiedostoon kirjoittaminen
             File.WriteAllText(tiedosto, Lampotila.ToString());
             Console.WriteLine("Uusi lämpötila tallennettu");
 
